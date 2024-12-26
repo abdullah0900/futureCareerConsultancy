748f972db4fa306b77f2ab01cc45eeb86ab3b33e
Abdullah Younas <81967040+abdullah0900@users.noreply.github.com>
2024-12-26 12:06:59 +0500

    Update

--- src/forms/contact.php
+++ src/forms/contact.php
@@ -1,12 +1,7 @@
 <?php  include("contactform.php");
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  
 
-  // Replace contact@example.com with your real receiving email address
+  // email
   $receiving_email_address = 'aeproduction706@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
